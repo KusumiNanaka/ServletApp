f2814ff8b26fbde8b3e7b855cf22169bd38f8d42
Nanaka Kusumi <n.kusumi.sys21@morijyobi.ac.jp>
2022-08-24 10:11:11 +0900

    てすと

--- servlet01/src/scopedata/Account.java
+++ servlet01/src/scopedata/Account.java
@@ -3,13 +3,13 @@ package scopedata;
 import java.io.Serializable;
 
 public class Account implements Serializable{
-	public String id;
-	public String pass;
+	public String name;
 	public String mail;
-	public Account(String _id ,String _pass ,String _mail) {
-		id = _id; pass = _pass; mail = _mail;
+	public String pass;
+	public Account(String _name ,String _mail ,String _pass) {
+		name = _name; mail = _mail; pass = _pass;
 	}
-	public String getId() {return id;}
-	public String getPass() {return pass;}
+	public String getName() {return name;}
 	public String getMail() {return mail;}
+	public String getPass() {return pass;}
 }
--- servlet01/src/servlet/sarvlet_rg.java
+++ servlet01/src/servlet/sarvlet_rg.java
@@ -8,10 +8,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import model.RegisterLogic;
-import scopedata.Account;
 
 @WebServlet("/sarvlet_rg")
 public class sarvlet_rg extends HttpServlet {
@@ -19,36 +15,11 @@ public class sarvlet_rg extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		String action = request.getParameter("action");
-		if(action == null) {
 			RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/u_reg.jsp");
 			dispatcher.forward(request, response);
-		}else {
-			HttpSession session = request.getSession();
-			Account account = (Account)session.getAttribute("LoginUser");
-			RegisterLogic rg = new RegisterLogic();
-			boolean r = rg.execute(account);
-			String path;
-			if(r == true)
-				path = "WEB-INF/jsp/u_check.jsp";
-			else
-				path = "#";
-
-			RequestDispatcher dispatcher = request.getRequestDispatcher(path);
-			dispatcher.forward(request, response);
 		}
-	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
-		String id = request.getParameter("id");
-		String pass = request.getParameter("pass");
-		String mail = request.getParameter("mail");
 
-		HttpSession session = request.getSession();
-		Account account = new Account(id ,pass ,mail);
-		session.setAttribute("LoginUser", account);
-
-		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/u_reg.jsp");
-		dispatcher.forward(request, response);
 	}
 }
